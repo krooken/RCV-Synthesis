ebf27b1797577aab93282467ca3c0be28c8db4ff
Jonas Krook <jonas.krook@zenuity.com>
2019-11-13 14:18:53 +0100

    Adding aeb to synthesis.
    Adding aeb to the always available upp2 in simple_upp2.py, and adding aeb to the full synthesis.

--- tulip/simple_aeb.py
+++ tulip/simple_aeb.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 13 10:03:06 2019
+Created on Wed Nov 13 11:24:37 2019
 
 @author: jonkro
 """
 
 from tulip import spec, synth
 import pickle
-
 
 #%%
 
@@ -17,8 +16,6 @@ env_vars = {
     'emergency_stopped': 'boolean',
     'sensor_failure': 'boolean',
     'driving': 'boolean',
-    'upp_2_response': ['none', 'success', 'failure'],
-    'upp_2_failed': 'boolean',
     'sstp_available': (0,1),
 }
 
@@ -28,19 +25,12 @@ env_init = {
     '!emergency_stopped',
     '!sensor_failure',
     '!driving',
-    'upp_2_response = "none"',
-    '!upp_2_failed',
     'sstp_available = 1',
 }
 
 env_safe = {
     "sensor_failure -> sensor_failure'",
-    '''!upp_2_request -> (upp_2_response' = "none")''',
-    '''upp_2_request -> 
-        ((upp_2_response' = "success" | upp_2_response' = "failure"))''',
-    '''(upp_2_response = "failure") -> upp_2_failed' ''',
-    '''(upp_2_response != "failure") -> (upp_2_failed'<->upp_2_failed) ''',
-    '''active_path = "sstp" -> sstp_available' > 0 ''',
+    ''' active_path = "sstp" -> sstp_available' > 0 ''',
     '''(!(driving & active_path="upp2") & !goal) -> !goal' ''',
     '''(!driving & goal) -> goal' ''',
     '''(!driving & safe_stopped) -> safe_stopped' ''',
@@ -48,7 +38,7 @@ env_safe = {
     '''(!driving & emergency_stopped) -> emergency_stopped' ''',
     '''(active_path!="aeb" & !emergency_stopped) -> !emergency_stopped' ''',
     '''driving' -> active_path != "none"''',
-    '''goal -> !driving''',
+    'goal -> !driving',
     'safe_stopped -> !driving',
     'emergency_stopped -> !driving',
     '''(!driving & active_path = "sstp") -> safe_stopped' ''',
@@ -56,45 +46,35 @@ env_safe = {
 }
 
 env_prog = {
-    '(active_path!="upp2" | sensor_failure | upp_2_failed) | goal',
+    '(active_path!="upp2" | sensor_failure) | goal',
     '(active_path!="sstp" | sstp_available = 0) | safe_stopped',
     'active_path!="aeb" | emergency_stopped',
 }
 
-
 #%%
 
 sys_vars = {
-    'upp_2_request': 'boolean',
     'active_path': ['none', 'upp2', 'sstp', 'aeb'],
-    'upp_2_available': 'boolean',
 }
 
 sys_init = {
-    '!upp_2_request',
     'active_path = "none"',
-    '!upp_2_available',
 }
 
 sys_safe = {
-    "upp_2_request -> !upp_2_request'",
-    '''(upp_2_response = "success") -> upp_2_available' ''',
-    '''(upp_2_response != "success") -> (upp_2_available' <-> upp_2_available) ''',
-    '''active_path = "upp2" -> (upp_2_available | upp_2_response = "success")''',
     '''active_path="sstp" -> sstp_available > 0''',
-    '''safe_stopped -> (sensor_failure | upp_2_failed)''',
+    '''safe_stopped -> (sensor_failure)''',
     '''safe_stopped -> !goal''',
-    '''emergency_stopped -> (sensor_failure | upp_2_failed)''',
+    '''emergency_stopped -> (sensor_failure)''',
     '''emergency_stopped -> !goal''',
     '''emergency_stopped -> !safe_stopped''',
-    '''(active_path = "upp2") -> active_path' != "none" ''',
+    '''(active_path = "upp2") -> active_path' != "none" '''
 }
 
 sys_prog = {
     'goal | safe_stopped | emergency_stopped',
     '!driving',
 }
-
 
 #%%
 
@@ -106,7 +86,6 @@ specs.qinit = '\E \A'
 specs.moore = False
 specs.plus_one = False
 
-
 #%%
 
 ctrl = synth.synthesize(specs, ignore_sys_init=False, ignore_env_init=False)
@@ -114,10 +93,10 @@ ctrl = synth.synthesize(specs, ignore_sys_init=False, ignore_env_init=False)
 #%%
 
 ctrl._transition_dot_label_format['separator'] = r'\n'
-ctrl.save('supervisor.pdf')
+ctrl.save('supervisor_simple_aeb.pdf')
 
 
 #%%
 
-with open('synthesized_controller.pickle', 'wb') as f:
+with open('synthesized_controller_simple_aeb.pickle', 'wb') as f:
     pickle.dump(ctrl, f)
